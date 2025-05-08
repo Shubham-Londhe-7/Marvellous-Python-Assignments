e6a52df620bcbf8214d9fd37ede2bc966abac87b
Shubham-Londhe-7 <shubhamlondhe976@gmail.com>
2025-05-08 17:46:06 +0530

    Add files via upload

---
+++ Assignment_1/Assignment_1_1.py
@@ -0,0 +1,10 @@
+# funcetin name Fun() that display "Hello from fun"
+
+def Fun():
+    print("Hello from fun")
+
+def main():
+    Fun()
+
+if __name__ == "__main__":
+    main()
---
+++ Assignment_1/Assignment_1_8.py
@@ -0,0 +1,14 @@
+# accept number and " * " that number of times
+
+def display(value):
+    for i in range(value):
+        print("*", end = " ")
+
+def main():
+    print("Enter Number : ")
+    No = int(input())
+
+    display(No)
+
+if __name__ == "__main__":
+    main()
