67ca9a255febd8a3d404d68d67975716003a97ed
Shubham-Londhe-7 <shubhamlondhe976@gmail.com>
2025-05-18 14:39:29 +0530

    Add files via upload

---
+++ Assignment_6/Assignment6_3.py
@@ -0,0 +1,10 @@
+
+def main():
+    print("Enter number : ")
+    no = int(input())
+
+    for i in range(1,11):
+        print(no,"*",i,"=", no * i)
+
+if __name__ == "__main__":
+    main()
---
+++ Assignment_6/Assignment6_6.py
@@ -0,0 +1,10 @@
+
+def main():
+    for i in range(4):
+        for j in range(4):
+            if (i >= j):
+                print("*",end = " ")
+        print()
+
+if __name__ == "__main__":
+    main()
